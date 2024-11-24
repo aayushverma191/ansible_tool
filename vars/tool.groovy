97bfb969d6ff7352aa41a1eb525122860f7c1d99
aayushverma191 <aayushverma4481@gmail.com>
2024-11-24 22:30:44 +0530

    Create tool.groovy

---
+++ vars/tool.groovy
@@ -0,0 +1,56 @@
+def call(String branch, String repoUrl) {
+
+pipeline {
+    agent any
+    
+    tools {
+        ansible 'ansible'
+    }
+    
+    // environment {
+    //    ANSIBLE_ROLES_PATH = "${WORKSPACE}/roles" // Set roles path dynamically
+    //}
+    
+        //-u ${ANSIBLE_USER} --extra-vars "ansible_password=${ANSIBLE_PASSWORD}" 
+    
+    stages{
+        stage('clone'){
+            steps{
+               // echo "git......!! ${WORKSPACE} "
+                  echo "Cloning repository..."
+                    checkout([$class: 'GitSCM',
+                              branches: [[name: "*/${branch}"]],
+                              userRemoteConfigs: [[url: repoUrl]]])
+               // git branch: 'main' , url: 'https://github.com/aayushverma191/ansible_tool.git'
+            }
+        }
+        stage('User_Approval'){
+            steps{
+                input message: 'do you want to install MYSQL', ok: 'Approved'
+                echo "Approval Done "
+            }
+        }
+        stage('Playbook_Execution'){
+            steps {
+                script{
+                     //withCredentials([usernamePassword(credentialsId: 'ansible-credentials', 
+                     //usernameVariable: 'ANSIBLE_USER', passwordVariable: 'ANSIBLE_PASSWORD')]) 
+                    //{
+                        sh """
+                            ansible-playbook -i ${WORKSPACE}/inventory ${WORKSPACE}/tool.yml --private-key ${WORKSPACE}/tool/ninja.pem
+                        """
+                    //}
+                }
+            }
+        }
+    }
+   post {
+         success {
+                 slackSend(channel: 'info', message: "Build Successful: JOB-Name:- ${JOB_NAME} Build_No.:- ${BUILD_NUMBER} & Build-URL:- ${BUILD_URL}")
+             }
+         failure {
+                 slackSend(channel: 'info', message: "Build Failure: JOB-Name:- ${JOB_NAME} Build_No.:- ${BUILD_NUMBER} & Build-URL:- ${BUILD_URL}")
+             }
+     }
+}
+}
